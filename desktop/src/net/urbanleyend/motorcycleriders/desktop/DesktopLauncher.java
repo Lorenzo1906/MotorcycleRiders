86a5fee8874fb8066caace6cd5c38cbdc74d3496
Lorenzo Rodriguez <Lorenzo1906@gmail.com>
2015-10-31 18:00:47 -0600

    Initial commit

---
+++ desktop/src/net/urbanleyend/motorcycleriders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.urbanleyend.motorcycleriders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.urbanleyend.motorcycleriders.MotorcycleRiders;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MotorcycleRiders(), config);
+	}
+}
